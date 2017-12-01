66dac064df0eb2dd78b27e6ed460055fc4eab691
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2017-12-01 15:31:15 +0200

    Table beta 0.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,16 @@
 import React, {Component} from 'react'
-// import Table from "./Table";
+import Table from "./Table";
 
 const containerStyles = {
-    border: '1px solid black',
-    padding: '40px 90px',
-    height: '70vh',
-    width: '170vh',
-
-};
-const tableStyle = {
+    padding: '40px 70px',
 };
 
-const bodyStyle = {
-};
-
-const cellStyle = {
-    border: '1px solid black',
-    height: '100px',
-    width: '1200px',
-};
 
 export default class App extends Component {
     render() {
         return (
             <div style={containerStyles}>
-                <table style={tableStyle}>
-                    <thead>
-
-                    </thead>
-                    <tbody style={bodyStyle}>
-                    <tr>
-                        <td style={cellStyle}>{}</td>
-                    </tr>
-                    <tr>
-                        <td style={cellStyle}>{}</td>
-                    </tr>
-                    <tr>
-                        <td style={cellStyle}>{}</td>
-                    </tr>
-                    <tr>
-                        <td style={cellStyle}>{}</td>
-                    </tr>
-                    <tr>
-                        <td style={cellStyle}>{}</td>
-                    </tr>
-                    <tr>
-                        <td style={cellStyle}>{}</td>
-                    </tr>
-                    </tbody>
-
-                </table>
+                <Table></Table>
             </div>
         );
     }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,58 +1,51 @@
 import React, {Component} from 'react'
 
-const tableStyles = {};
-const tableHead = {};
-const tableBody = {};
-const teamRow = {};
-const playerRow = {};
+const bodyStyle = {};
+
+const cellStyle = {
+    border: '2px solid dimgrey',
+    height: '100%',
+    width: '33.34%',
+    borderTop: 'white',
+    borderBottom: 'white',
+};
+const cellNullStyle = {
+    border: '2px solid dimgrey',
+    borderTop: 'white',
+    borderBottom: 'white',
+    width: '0',
+};
+const tableStyle = {
+    borderSpacing: '2px',
+    height: '500px',
+    width: '100%',
+};
 
 /*
 * teams: int
 * playersPerTeam: arr(int) : [4, 4, 3, ...]
 * */
 export default class Table extends Component {
-    //
-    // public teams;
-    // public playersPerTeam;
-    //
-    //
-    // componentWillMount() {
-    //
-    // }
-    //
-    // playerRows = () => {
-    //
-    //     let rows = [];
-    //     for (let i = 0; i < this.teams; i++) {
-    //         rows.push(
-    //             <tr key={i} style={teamRow}>
-    //                 {this.playersPerTeam.map((members, index) => {
-    //                     return
-    //                     <tr style={playerRow} key={index}>
-    //                         <td>
-    //
-    //                         </td>
-    //                     </tr>
-    //                 })}
-    //             </tr>)
-    //     }
-    //     return rows;
-    // };
-    //
-    // render() {
-    //     return (
-    //         <div style={tableStyles}>
-    //             <div>
-    //                 <table>
-    //                     <tr style={tableHead}>
-    //
-    //                     </tr>
-    //                     <tr style={tableBody}>
-    //                         {this.playerRows()}
-    //                     </tr>
-    //                 </table>
-    //             </div>
-    //         </div>
-    //     );
-    // }
+
+
+    render() {
+        return (
+            <div>
+                <table style={tableStyle}>
+                    <thead>
+
+                    </thead>
+                    <tbody style={bodyStyle}>
+                    <tr>
+                        <td style={cellStyle}>{}</td>
+                        <td style={cellStyle}>{}</td>
+                        <td style={cellNullStyle}>{}</td>
+                        <td style={cellStyle}>{}</td>
+                    </tr>
+                    </tbody>
+
+                </table>
+            </div>
+        );
+    }
 }
